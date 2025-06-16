a1c7b5645d4e98e06d1d297e12dfcddfa7a8937c
JSander72 <jamesearlsanders2@outlook.com>
2025-06-16 08:46:40 -0400

    second commit

--- personal-portfolio/src/components/NavBar.js
+++ personal-portfolio/src/components/NavBar.js
@@ -1,8 +1,18 @@
 
 import { useState,useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg'; 
+import { HashLink } from 'react-router-hash-link';
+import {
+    BrowserRouter as Router
+  } from "react-router-dom";
+
 
 export const NavBar = () => {
+    
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
 
@@ -29,7 +39,7 @@ export const NavBar = () => {
         <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
         <Container>
             <Navbar.Brand href="#home">
-                <img src={""} alt="Logo" />
+                <img src={logo} alt="Logo" />
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" >
                 <span className="navbar-toggler-icon"></span>
@@ -42,9 +52,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
                 <div className="social-icon">
-                <a href="#"><img src={} alt="" /></a> 
-                <a href="#"><img src={} alt="" /></a>
-                <a href="#"><img src={} alt="" /></a>
+                <a href="#"><img src={navIcon1} alt="" /></a> 
+                <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="#"><img src={navIcon3} alt="" /></a>
                 </div>
                 <button className="vvd" onClick={() => console.log('connect')}><span>Let’s Connect</span></button>
                 </span>
